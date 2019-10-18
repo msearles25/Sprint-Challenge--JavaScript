7c7d069640b51cd146193f4fc7f577a357d5831d
msearles25 <skaman4459@gmail.com>
2019-10-18 12:33:12 -0400

    functions done

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -80,7 +80,7 @@ const graduates = [
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-// for(i = 0; i < graduates.length; i++) [
+// for(let i = 0; i < graduates.length; i++) [
 //   universities.push(graduates[i].university)
 // ]
 // universities.sort();
@@ -150,7 +150,7 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
+const lowPopulationAnimals = zooAnimals.filter((animal) => animal.population < 5);
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -158,7 +158,10 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce(function(accum, animal) {
+  return accum + animal.population;
+}, 0)
+
 console.log(populationTotal);
 
 
